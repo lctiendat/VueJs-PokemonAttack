973ab9561db776e7c831028a465a3017b6cf5e94
lctiendat <lctiendat@lctiendats-MacBook-Pro.local>
2021-07-29 18:08:48 +0700

    ver2.0

--- main.js
+++ main.js
@@ -6,6 +6,24 @@ new Vue({
     gameIsRunning: false,
     logsGame: [],
     isLogs: false,
+    items: [
+      {
+        name: "HP",
+        chiso: 5,
+        quality: 5,
+        tien: 10,
+        thumbail: "./images/hp.png",
+      },
+      {
+        name: "Def",
+        chiso: 10,
+        quality: 5,
+        tien: 10,
+        thumbail: "./images/shield.png",
+      },
+    ],
+    cash: 100,
+    defSatoshi: 0,
   },
   methods: {
     startGame() {
@@ -13,39 +31,47 @@ new Vue({
       this.resetHeight();
       this.isLogs = true;
       this.logsGame = [];
+      this.cash = 100;
+      this.defSatoshi = 0;
     },
     finalGame() {
       this.gameIsRunning = false;
       this.resetHeight();
       this.isLogs = false;
       this.logsGame = [];
+      this.cash = 10;
     },
     resetHeight() {
       this.satoshiHeight = 100;
       this.pikachuHeight = 100;
     },
     attack() {
-      this.satoshiAttack();
-      this.pikachuAttack();
       this.playerCheckOption();
       var satoshiDame = this.satoshiAttack();
       var pikachuDame = this.pikachuAttack();
-      this.logsGame.unshift(
-        "Satoshi attack " + satoshiDame + " height Pikachu"
-      );
-      this.logsGame.unshift(
-        "Pikachu attack " + pikachuDame + " height Satoshi"
-      );
+      // this.logsGame.unshift("Satoshi tấn công " + satoshiDame + " máu Pikachu");
+      // this.logsGame.unshift("Pikachu tấn công " + pikachuDame + " máu Satoshi");
+      document.getElementById("resultSatoshi").innerHTML =
+        " - " + pikachuDame + " HP";
+      document.getElementById("resultPikachu").innerHTML =
+        " - " + satoshiDame + " HP";
       this.logGameAttack();
     },
     satoshiAttack() {
       var dame = Math.floor(Math.random() * 10);
-      this.satoshiHeight -= Math.floor(Math.random() * 10);
+      this.pikachuHeight -= dame;
       return dame;
     },
     pikachuAttack() {
       var dame = Math.floor(Math.random() * 10);
-      this.pikachuHeight -= Math.floor(Math.random() * 10);
+      var hasDef = dame - 5;
+      if (this.defSatoshi > 0) {
+        this.satoshiHeight -= hasDef;
+        this.defSatoshi -= 5;
+      } else {
+        this.satoshiHeight -= dame;
+      }
+      console.log(this.defSatoshi);
       return dame;
     },
     satoshiSpecialAttack() {
@@ -59,34 +85,35 @@ new Vue({
       return dame;
     },
     specialAttack() {
-      this.satoshiSpecialAttack();
-      this.pikachuSpecialAttack();
       this.playerCheckOption();
       var satoshiDame = this.satoshiSpecialAttack();
       var pikachuDame = this.pikachuSpecialAttack();
-      this.logsGame.unshift(
-        "Satoshi attack " + satoshiDame + " height Pikachu"
-      );
-      this.logsGame.unshift(
-        "Pikachu attack " + pikachuDame + " height Satoshi"
-      );
+      // this.logsGame.unshift("Satoshi tấn công " + satoshiDame + " máu Pikachu");
+      // this.logsGame.unshift("Pikachu tấn công " + pikachuDame + " máu Satoshi");
+      document.getElementById("resultSatoshi").innerHTML =
+        " - " + pikachuDame + " HP";
+      document.getElementById("resultPikachu").innerHTML =
+        " - " + satoshiDame + " HP";
       this.logGameAttack();
     },
     heal() {
       if (this.satoshiHeight > 70) {
-        alert("Height hight , no heal");
+        alert("Máu quá cao, không thể hồi");
       } else if (this.satoshiHeight <= 60) {
         this.satoshiHeight += 10;
-        this.pikachuAttack();
-        var pikachuDame = this.pikachuAttack();
-        this.logsGame.unshift("Satoshi heal 10 hp");
-        this.logsGame.unshift(
-          "Pikachu attack " + pikachuDame + " height Satoshi"
-        );
+        document.getElementById("resultSatoshi").innerHTML = " + 10 HP";
+        // this.pikachuAttack();
+        // var pikachuDame = this.pikachuAttack();
+        // this.logsGame.unshift("Satoshi đã hồi 10 HP");
+        // this.logsGame.unshift(
+        //   "Pikachu tấn công " + pikachuDame + " máu Satoshi"
+        // );
+        document.getElementById("resultPikachu").innerHTML = "";
         document.getElementById("heal").disabled = true;
       } else {
         this.satoshiHeight = 70;
       }
+
       this.logGameAttack();
       setTimeout(() => {
         document.getElementById("heal").disabled = false;
@@ -99,17 +126,21 @@ new Vue({
     },
     playerCheckOption() {
       if (this.pikachuHeight <= 0) {
-        if (confirm("You win  ! You are ready")) {
+        if (confirm("Bạn thắng rồi ! Bạn có muốn chơi lại không")) {
           this.finalGame();
+          this.resetItems();
         } else {
           this.finalGame();
+          this.resetItems();
         }
       }
       if (this.satoshiHeight <= 0) {
-        if (confirm("You lose  ! You are ready")) {
+        if (confirm("Bạn thua rồi ! Bạn có muốn chơi lại không ?")) {
           this.finalGame();
+          this.resetItems();
         } else {
           this.finalGame();
+          this.resetItems();
         }
       }
     },
@@ -118,18 +149,46 @@ new Vue({
         this.logsGame.splice(2, 2);
       }
     },
+    buyItem(index) {
+      this.items.forEach((item, i) => {
+        var name = item.name;
+        if (i == index) {
+          if (this.cash > 0) {
+            if (item.quality > 0) {
+              // this.checkCash();
+              if (index == 1) {
+                this.defSatoshi += item.chiso;
+                item.quality -= 1;
+                this.cash -= item.tien;
+              } else {
+                if (this.satoshiHeight <= 70) {
+                  this.satoshiHeight += item.chiso;
+                  item.quality -= 1;
+                  this.cash -= item.tien;
+                } else {
+                  alert("Máu của bạn chưa đủ điều kiện để mua item");
+                }
+              }
+            } else {
+              alert("Item " + name + " đã hết hàng");
+              return false;
+            }
+          } else {
+            alert("Bạn đã hết tiền");
+            return false;
+          }
+        }
+      });
+    },
+    resetItems() {
+      this.items.forEach((item, i) => {
+        item.quality = 5;
+      });
+    },
+    addCoin() {
+      alert(
+        "Có cái nịt, dù add thêm được coin nhưng tui giới hạn số lượng item rồi, add làm gì :v"
+      );
+    },
   },
-  // watch: {
-  //   pikachuHeight() {
-  //     if (this.pikachuHeight <= 0) {
-  //       alert("win");
-  //       document.querySelector();
-  //     }
-  //   },
-  //   satoshiHeight() {
-  //     if (this.satoshiHeight <= 0) {
-  //       alert("lose");
-  //     }
-  //   },
-  // },
 });
